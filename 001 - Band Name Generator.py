1dcaa17853a141183bf551a4bd417ebb3b4fd147
Hassan Aziz <47973@students.riphah.edu.pk>
2024-05-14 22:21:37 +0500

    Added final project of Day 01
    In this project, I am using variables to create a simple program that generates a "Band Name" for users.

---
+++ 001 - Band Name Generator.py
@@ -0,0 +1,5 @@
+print("Welcome to this program that generates a stupid name for your band!")
+city = input("Which city were you born in?\n")
+pet = input("What is the name of your favorite pet?\n")
+print("\nThe name of your band could be " + city + ' ' + pet)
+print(f"And your band initials are: '{city[0]}. {pet[0]}'")
