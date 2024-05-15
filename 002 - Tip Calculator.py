970f15efa35ecf28c4215f2ee143871532cfe51c
Hassan Aziz <47973@students.riphah.edu.pk>
2024-05-15 19:30:49 +0500

    Added Day 02
    Added some intermediate exercises for day 2 as well. In the main project, I am taking input from user and also performing type conversion between different data types.

---
+++ 002 - Tip Calculator.py
@@ -0,0 +1,17 @@
+print("Welcome to the tip calculator.")
+
+bill = float(input("What is the total bill? $ "))
+
+tip = int(input("What percentage of tip would you like to give? 10, 12 or 15? "))
+
+people = int(input("How many people will split the bill? "))
+
+
+tip_as_percent = tip / 100
+total_tip_amount = bill * tip_as_percent
+total_bill = bill + total_tip_amount
+bill_per_person = total_bill / people
+# final_amount = round(bill_per_person, 2)
+final_amount = "{:.2f}".format(bill_per_person)
+
+print(f"Each person should pay: Rs. {final_amount}")
---
+++ Intermediate Exercises/day 2.py
@@ -0,0 +1,10 @@
+two_digit_number = input("Enter a two digit number : ")
+
+
+num1 = int(two_digit_number[0])
+
+num2 = int(two_digit_number[1])
+
+num_sum = num1 + num2
+
+print(str(num_sum))
